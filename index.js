0a5a20605036e26b8fa67528733ee28c4317cef1
asakusuma <asa.kusuma@gmail.com>
2015-05-22 13:15:21 -0700

    Attempt to propogate ember-cli hooks to children

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 /* jshint node: true */
 'use strict';
 
+var eb = require('ember-browserify');
+
 module.exports = {
-  name: 'uses-ember-browserify'
+  name: 'uses-ember-browserify',
+  included: function() {
+    eb.included.apply(this, arguments);
+  },
+  postprocessTree: function() {
+    eb.postprocessTree.apply(this, arguments);
+  }
 };
